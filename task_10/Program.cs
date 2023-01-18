8e5c2a561141a7ef6c3cc97ea4ea9b214e910e50
roman1231341414 <120581429+roman1231341414@users.noreply.github.com>
2023-01-18 22:26:52 +0300

    На всякий случай

---
+++ task_10/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+// A(3,6); B(2,1) -> 5,09
+// A(7,-5); B(1,-1) -> 7,21
+
+Console.Clear();
+int getUserValue(string message)
+{
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+double getDistanceFromCoordinate(int userAx,int userAy,int userBx,int userBy)
+{
+    double result = Math.Sqrt(Math.Pow((userAx-userBx),2) + Math.Pow((userAy-userBy),2));
+    return result;
+}
+int userAx = getUserValue("Введите X");
+int userAy = getUserValue("Введите y");
+
+int userBx = getUserValue("Введите X");
+int userBy = getUserValue("Введите y");
+
+double distance = getDistanceFromCoordinate(userAx,userAy,userBx,userBy);
+Console.WriteLine($"расстояние {distance}");
---
+++ task_22/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход число N и выдает таблицу квадратов чисел 
+// от 1 до N 
+
+Console.Clear();
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int i = 1;
+
+while(i<= N)
+{
+    Console.Write(Math.Pow(i,2)+", ");
+    i++;
+}
