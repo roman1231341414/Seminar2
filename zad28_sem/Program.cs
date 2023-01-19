f942c383f279945746caaf5561f48cc8367e3e4c
roman1231341414 <120581429+roman1231341414@users.noreply.github.com>
2023-01-19 18:38:56 +0300

    задачи 29

---
+++ zad28_sem/Program.cs
@@ -0,0 +1,21 @@
+﻿// N? от 1 до N произведение
+
+int getUserData(string message)
+{
+    Console.WriteLine(message);
+    int userData = int.Parse(Console.ReadLine());
+    return userData;
+}
+
+int getProductOfNumbers(int number)
+{
+    int sum = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        sum = sum * i;
+    }
+    return sum;
+}
+int number = getUserData("Введите число N ");
+int sum = getProductOfNumbers(number);
+Console.WriteLine($"Произведение число от 1 до N = {sum}");
---
+++ zad30_sem/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+int [] GetArrayRandom(int length)
+{
+    int[] arr = new int[length];
+   for(int i = 0; i < length; i++)
+   {
+    arr[i] = new Random().Next(0,2);
+   } 
+   return arr;
+}
+
+int[] array = GetArrayRandom(8);
+
+void showArray(int[] array)
+{
+    Console.Write($"[");
+ for(int i = 0; i <array.Length; i++)
+ {
+    if(i != array.Length - 1)
+    {
+        Console.Write($"{array[i]},");
+    }
+    else 
+    {
+        Console.Write($"{array[i]}]");
+    }
+    
+ }
+}
+
+showArray(array);
