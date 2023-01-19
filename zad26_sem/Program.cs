559b660ac697b0434772d06fb146b357c6fc865a
roman1231341414 <120581429+roman1231341414@users.noreply.github.com>
2023-01-19 15:08:27 +0300

    домашнее задание задача 24/26

---
+++ zad26_sem/Program.cs
@@ -0,0 +1,21 @@
+﻿int getUserData(string message)
+{
+    Console.WriteLine(message);
+    int userData = int.Parse(Console.ReadLine());
+    return userData;
+}
+
+int getNumberOfDigit(int number)
+{
+    int numberOfDigit = 0;
+    while(number > 0)
+    {
+     number = number / 10;
+     numberOfDigit++;
+    }
+    return numberOfDigit;
+}
+
+int number = getUserData("Введите число");
+int numberOfDigit = getNumberOfDigit(number);
+Console.WriteLine($"В числе {number} = {numberOfDigit}");
