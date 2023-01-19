2d5dc271bad0de11b0a5198d47a2b8b0df5042b2
roman1231341414 <120581429+roman1231341414@users.noreply.github.com>
2023-01-19 15:09:13 +0300

    домашнее задание задача 24/26/2

---
+++ Lex_1/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+
+Console.Clear();
+int[] arr = {1,5,4,3,2,6,7,1,1};
+
+void PrintArray(int[] array)
+{
+   int count = array.Length;
+   for(int i = 0;i < count;i++)
+   {
+     Console.Write($"{array[i]} ");
+   }
+   Console.WriteLine();
+}
+
+void SelectionSort(int [] array)
+{
+  for(int i = array.Length - 1; i > 0; i--)
+  {
+    int maxPosition = i;
+    for(int j = i - 1; j > 0;j--)
+    {
+      if(array[j] > array[maxPosition]) maxPosition = j;
+    }
+    int temporary = array[i];
+    array[i] = array[maxPosition];
+    array[maxPosition] = temporary;
+  }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
---
+++ zad24_sem/Program.cs
@@ -0,0 +1,22 @@
+﻿int getUserData(string message)
+{
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int userData = int.Parse(Console.ReadLine()!);
+    return userData;
+}
+
+int getSumOfRange(int start, int end)
+{
+    int sum = 0;
+    for (int i = start; i <= end; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+int end = getUserData("Введите число A для получения суммы");
+int sum = getSumOfRange(1,end);
+Console.WriteLine(sum);
