822a502716ea6dce11a232bb5669ed5fdedf6dbb
roman1231341414 <120581429+roman1231341414@users.noreply.github.com>
2023-01-19 19:23:43 +0300

    задачи 27

---
+++ zad25/Program.cs
@@ -0,0 +1,23 @@
+﻿
+int userData(string msg)
+{
+    Console.WriteLine(msg);
+    int user = int.Parse(Console.ReadLine());
+    return user;
+}
+
+int exponentiation(int A, int B)
+{
+    int sum = 1;
+  for(int i = 0; i < B; i++)
+  {
+    sum = sum * A;
+
+  }
+  return sum;
+}
+
+int A = userData("Введите число А ");
+int B = userData("Введите число B ");
+int sum  = exponentiation( A, B);
+Console.WriteLine($"число {A} в степени {B} = {sum}");
---
+++ zad27/Program.cs
@@ -0,0 +1,33 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12
+*/
+
+int userData(string msg)
+{
+    Console.WriteLine(msg);
+    int user = int.Parse(Console.ReadLine());
+    return user;
+}
+
+int skannumbers(int number)
+{
+    int counter = Convert.ToString(number).Length;
+    int advance = 0;
+    int result = 0;
+    for (int i = 0; i < number; i++)
+    {
+      advance = number - number% 10;
+      result = result + (number - advance);
+      number = number / 10;
+    }
+    return result;
+}
+
+int number = userData("Введите число");
+int digit = skannumbers(number);
+Console.WriteLine($"Сумма цифр в числе {digit}");
